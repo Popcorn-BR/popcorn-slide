c9bbf7e5fa57ca21f63f1bfcc1ee9fb53850effc
Denner Rondinely <denner.rondinely@gmail.com>
2020-03-16 00:16:55 -0300

    WIP

--- tests/zoom.spec.js
+++ tests/zoom.spec.js
@@ -6,12 +6,7 @@ import Zoom from '../src/Zoom';
 chai.use(sinonChai);
 
 describe('Popcorn', () => {
-  const zoom = new Zoom({
-    canvas: null,
-    list: [],
-    width: 1,
-    height: 1,
-  });
+  const zoom = new Zoom();
   describe('smoke test', () => {
     it('should have init method', () => {
       expect(zoom.init).to.exist;
@@ -35,6 +30,45 @@ describe('Popcorn', () => {
 
     it('should have getX method', () => {
       expect(zoom.getX).to.exist;
+    });
+  });
+
+  describe('Popcorn.Zoom.init', () => {
+    it('should have init method', () => {
+      const call = zoom.init();
+    });
+  });
+  describe('Popcorn.Zoom.zoomIn', () => {
+    it('should have zoomIn method', () => {
+      const call = zoom.zoomIn();
+    });
+  });
+
+  describe('Popcorn.Zoom.zoomOut', () => {
+    it('should have zoomOut method', () => {
+      const call = zoom.zoomOut();
+    });
+  });
+
+  describe('Popcorn.Zoom.updateParams', () => {
+    it('should have updateParams method', () => {
+      const call = zoom.updateParams();
+    });
+  });
+
+  describe('Popcorn.Zoom.getY', () => {
+    it('should have getY method', () => {
+      const call = zoom.getY();
+    });
+
+    it('should have getX method', () => {
+      const call = zoom.getX();
+    });
+  });
+
+  describe('Popcorn.Zoom.getX', () => {
+    it('should have getX method', () => {
+      const call = zoom.getX();
     });
   });
 });
